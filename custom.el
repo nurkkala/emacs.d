2fd2d0112c533b5634027f4658734f44d6b995fa
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2016-02-18 16:12:47 -0500

    Initial revision

---
+++ custom.el
@@ -0,0 +1,256 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-PDF-mode t t)
+ '(TeX-view-program-list (quote (("Skim" "open -a Skim %o"))))
+ '(TeX-view-program-selection
+   (quote
+	((output-pdf "Skim")
+	 ((output-dvi style-pstricks)
+	  "dvips and gv")
+	 (output-dvi "xdvi")
+	 (output-pdf "Evince")
+	 (output-html "xdg-open"))))
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(bibtex-dialect (quote biblatex))
+ '(coffee-tab-width 4)
+ '(column-number-mode t)
+ '(confirm-kill-emacs (quote yes-or-no-p))
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+	("68d36308fc6e7395f7e6355f92c1dd9029c7a672cbecf8048e2933a053cf27e6" "0ad5a61e6ee6d2e7f884c0da7a6f437a4c84547514b509bdffd06757a8fc751f" "df3e05e16180d77732ceab47a43f2fcdb099714c1c47e91e8089d2fcf5882ea3" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" "11636897679ca534f0dec6f5e3cb12f28bf217a527755f6b9e744bd240ed47e1" "19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" "a444b2e10bedc64e4c7f312a737271f9a2f2542c67caa13b04d525196562bf38" "2e5705ad7ee6cfd6ab5ce81e711c526ac22abed90b852ffaf0b316aa7864b11f" "b06aaf5cefc4043ba018ca497a9414141341cb5a2152db84a9a80020d35644d1" "3dafeadb813a33031848dfebfa0928e37e7a3c18efefa10f3e9f48d1993598d3" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "1db337246ebc9c083be0d728f8d20913a0f46edc0a00277746ba411c149d7fe5" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "6a9606327ecca6e772fba6ef46137d129e6d1888dcfc65d0b9b27a7a00a4af20" default)))
+ '(desktop-save-mode t)
+ '(display-time-mode nil)
+ '(elpy-mode-hook (quote (hl-line-mode)))
+ '(emmet-indentation 2)
+ '(f90-auto-keyword-case (quote upcase-word))
+ '(fci-rule-color "#383838")
+ '(fill-column 79)
+ '(gnutls-min-prime-bits 1024)
+ '(helm-man-or-woman-function (quote woman))
+ '(helm-reuse-last-window-split-state t)
+ '(ibuffer-formats
+   (quote
+	((mark modified read-only " "
+		   (name 18 18 :left :elide)
+		   " "
+		   (size 9 -1 :right)
+		   " "
+		   (mode 16 16 :left :elide)
+		   " " filename-and-process)
+	 (mark modified read-only " "
+		   (name 25 25 :left :elide)
+		   " "
+		   (size 8 -1 :right)
+		   " "
+		   (mode 10 10)
+		   " " filename-and-process))))
+ '(ibuffer-saved-filter-groups
+   (quote
+	(("Tom's Filter Group"
+	  ("Python"
+	   (mode . python-mode))
+	  ("JavaScript"
+	   (mode . js2-mode))
+	  ("HTML/Web"
+	   (or
+		(used-mode . html-mode)
+		(used-mode . css-mode)
+		(used-mode . web-mode)))))))
+ '(ibuffer-saved-filters
+   (quote
+	(("gnus"
+	  ((or
+		(mode . message-mode)
+		(mode . mail-mode)
+		(mode . gnus-group-mode)
+		(mode . gnus-summary-mode)
+		(mode . gnus-article-mode))))
+	 ("programming"
+	  ((or
+		(mode . emacs-lisp-mode)
+		(mode . cperl-mode)
+		(mode . c-mode)
+		(mode . java-mode)
+		(mode . idl-mode)
+		(mode . lisp-mode)))))))
+ '(ido-enable-tramp-completion nil)
+ '(indicate-buffer-boundaries (quote left))
+ '(indicate-empty-lines t)
+ '(jedi:complete-on-dot t)
+ '(jedi:setup-keys t)
+ '(jinja2-user-keywords (quote ("topic" "section" "subsection")))
+ '(js2-basic-offset 2)
+ '(magit-log-arguments (quote ("--graph" "--color" "--decorate")))
+ '(markdown-command
+   "/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin/markdown_py")
+ '(message-citation-line-function (quote message-insert-formatted-citation-line))
+ '(message-signature-directory "~/.signatures")
+ '(message-signature-file "nurknet.txt")
+ '(minimap-update-delay 0.4)
+ '(minimap-window-location (quote right))
+ '(mu4e-bookmarks
+   (quote
+	(("date:24h..now and flag:unread and not flag:trashed" "Last 24 Hours (Unread)" 50)
+	 ("date:48h..now and flag:unread and not flag:trashed" "Last 48 Hours (Unread)" 52)
+	 ("date:today..now and not flag:trashed" "Today (All)" 116)
+	 ("date:7d..now and flag:unread" "Last 7 Days (Unread)" 119)
+	 ("date:30d..now and flag:unread" "Last 30 Days (Unread)" 109)
+	 ("flag:unread AND NOT flag:trashed" "Unread (All)" 117))))
+ '(mu4e-compose-dont-reply-to-self t)
+ '(mu4e-compose-signature t)
+ '(mu4e-compose-signature-auto-include nil)
+ '(mu4e-get-mail-command "/opt/local/bin/offlineimap")
+ '(mu4e-headers-leave-behavior (quote apply))
+ '(mu4e-html2text-command "textutil -stdin -format html -convert txt -stdout")
+ '(mu4e-maildir "/Users/tom/Mail")
+ '(mu4e-mu-binary "/usr/local/bin/mu")
+ '(mu4e-update-interval 300)
+ '(mu4e-user-mail-address-list
+   (quote
+	("tom.nurkkala@gmail.com" "thnurkkala@taylor.edu" "tnurkkala@cse.taylor.edu" "tom@nurknet.com" "thnurkkala@tayloru.edu")))
+ '(mu4e-view-show-images t)
+ '(newsticker-url-list
+   (quote
+	(("Slashdot" "http://rss.slashdot.org/Slashdot/slashdot" nil nil nil))))
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(org-agenda-files
+   (quote
+	("~/Taylor/Classes/2013-2014/Spring/SYS 394 - ISD/schedule.org" "~/Org/todo.org")))
+ '(org-agenda-start-on-weekday 0)
+ '(org-babel-load-languages
+   (quote
+	((latex . t)
+	 (python . t)
+	 (sh . t)
+	 (emacs-lisp . t))))
+ '(org-checkbox-hierarchical-statistics nil)
+ '(org-clock-continuously nil)
+ '(org-clock-into-drawer t)
+ '(org-clock-out-remove-zero-time-clocks t)
+ '(org-confirm-babel-evaluate nil)
+ '(org-default-notes-file "~/Org/refile.org")
+ '(org-export-with-drawers t)
+ '(org-hierarchical-checkbox-statistics nil)
+ '(org-latex-classes
+   (quote
+	(("article" "\\documentclass[11pt]{article}"
+	  ("\\section{%s}" . "\\section*{%s}")
+	  ("\\subsection{%s}" . "\\subsection*{%s}")
+	  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+	  ("\\paragraph{%s}" . "\\paragraph*{%s}")
+	  ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+	 ("report" "\\documentclass[11pt]{report}"
+	  ("\\part{%s}" . "\\part*{%s}")
+	  ("\\chapter{%s}" . "\\chapter*{%s}")
+	  ("\\section{%s}" . "\\section*{%s}")
+	  ("\\subsection{%s}" . "\\subsection*{%s}")
+	  ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
+	 ("book" "\\documentclass[11pt]{book}"
+	  ("\\part{%s}" . "\\part*{%s}")
+	  ("\\chapter{%s}" . "\\chapter*{%s}")
+	  ("\\section{%s}" . "\\section*{%s}")
+	  ("\\subsection{%s}" . "\\subsection*{%s}")
+	  ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
+	 ("syllabus" "\\documentclass{article}"
+	  ("\\section{%s}" . "\\section*{%s}")
+	  ("\\subsection{%s}" . "\\subsection*{%s}")))))
+ '(org-log-done (quote note))
+ '(org-log-into-drawer t)
+ '(org-log-note-clock-out t)
+ '(org-mobile-files (quote (org-agenda-files "tenure-app.org")))
+ '(org-reveal-center nil)
+ '(org-reveal-history t)
+ '(org-reveal-margin "0.1")
+ '(org-reveal-mathjax t)
+ '(org-reveal-root "http://cdn.jsdelivr.net/reveal.js/2.5.0/")
+ '(org-reveal-theme "beige")
+ '(org-src-fontify-natively t)
+ '(org-src-preserve-indentation t)
+ '(org-startup-folded (quote content))
+ '(org-startup-indented t)
+ '(projectile-mode-line
+   (quote
+	(:eval
+	 (format " Proj[%s]"
+			 (projectile-project-name)))))
+ '(recentf-mode t)
+ '(reftex-plug-into-AUCTeX t)
+ '(reftex-ref-style-default-list (quote ("Default" "Hyperref")))
+ '(rw-hunspell-dicpath-list (quote ("/opt/local/share/hunspell")))
+ '(safe-local-variable-values
+   (quote
+	((TeX-master . qrs-tee-erd-hw)
+	 (engine . django)
+	 (pyvenv-workon . brook)
+	 (TeX-engine . "lualatex")
+	 (eval when
+		   (fboundp
+			(quote rainbow-mode))
+		   (rainbow-mode 1)))))
+ '(save-place t nil (saveplace))
+ '(send-mail-function (quote smtpmail-send-it))
+ '(show-paren-mode t)
+ '(show-trailing-whitespace t)
+ '(size-indication-mode t)
+ '(smtpmail-queue-dir "~/Mail/queue/cur")
+ '(smtpmail-queue-mail nil)
+ '(smtpmail-smtp-server "smtp.webfaction.com")
+ '(smtpmail-smtp-service 587)
+ '(speedbar-default-position (quote right))
+ '(tab-width 4)
+ '(tool-bar-mode nil)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(user-mail-address "tom@nurknet.com")
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+	((20 . "#BC8383")
+	 (40 . "#CC9393")
+	 (60 . "#DFAF8F")
+	 (80 . "#D0BF8F")
+	 (100 . "#E0CF9F")
+	 (120 . "#F0DFAF")
+	 (140 . "#5F7F5F")
+	 (160 . "#7F9F7F")
+	 (180 . "#8FB28F")
+	 (200 . "#9FC59F")
+	 (220 . "#AFD8AF")
+	 (240 . "#BFEBBF")
+	 (260 . "#93E0E3")
+	 (280 . "#6CA0A3")
+	 (300 . "#7CB8BB")
+	 (320 . "#8CD0D3")
+	 (340 . "#94BFF3")
+	 (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3")
+ '(visual-line-mode nil t)
+ '(web-mode-markup-indent-offset 2)
+ '(which-function-mode nil)
+ '(woman-fill-frame t)
+ '(woman-manpath
+   (quote
+	("/opt/local/share/man" "/usr/share/man" "/usr/local/share/man" "/usr/X11/man"
+	 ("/bin" . "/usr/share/man")
+	 ("/sbin" . "/usr/share/man")
+	 ("/usr/bin" . "/usr/share/man")
+	 ("/usr/sbin" . "/usr/share/man")
+	 ("/usr/local/bin" . "/usr/local/share/man")
+	 ("/usr/local/sbin" . "/usr/local/share/man")
+	 ("/usr/X11/bin" . "/usr/X11/man")
+	 ("/usr/bin/X11" . "/usr/X11/man")
+	 ("/usr/bin/mh" . "/usr/share/man")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo"))))
+ '(html-mode-default ((t (:inherit sgml-mode-default :height 140 :foundry "Monaco" :family "apple"))) t)
+ '(minimap-active-region-background ((t (:background "#555"))))
+ '(minimap-font-face ((t (:height 0.3 :family "DejaVu Sans Mono")))))
---
+++ init.el
@@ -0,0 +1,271 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Tom's Emacs Configuration of Doom
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Set the location of the customization settings.
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;; ELPA - Set up packages early so that they can be used from within this file.
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
+
+;; After ELPA package initialization, can use locally installed libraries.
+(require 's)							;String library
+
+;; Key bindings. From emacs manual: Sequences consisting of C-c and a letter (either upper
+;; or lower case) are reserved for users.
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c d") 'dash-at-point)
+(global-set-key (kbd "C-c e") 'emmet-expand-line)
+(global-set-key (kbd "C-c g") 'magit-status)
+(global-set-key (kbd "C-c i") 'mu4e)
+(global-set-key (kbd "C-c k e") 'external-keyboard)
+(global-set-key (kbd "C-c k i") 'internal-keyboard)
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c m") 'compile)
+(global-set-key (kbd "C-c s") 'sort-lines)
+(global-set-key (kbd "C-c t") 'sr-speedbar-toggle)
+(global-set-key (kbd "C-c u") 'untabify)
+(global-set-key (kbd "C-c w") 'delete-trailing-whitespace)
+(global-set-key (kbd "C-c y") 'bury-buffer)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+;; Disable ns-win binding of ns-print-buffer, which causes no end of grief.
+(global-unset-key (kbd "s-p"))
+
+;; Scroll modestly without moving point.
+(setq nurk/modest-scroll-lines 1)
+(global-set-key (kbd "M-n") (lambda () (interactive) (scroll-up nurk/modest-scroll-lines)))
+(global-set-key (kbd "M-p") (lambda () (interactive) (scroll-down nurk/modest-scroll-lines)))
+
+;; Modifier bindings
+(defun internal-keyboard ()
+  "Define modifers to suit internal keyboard."
+  (interactive)
+  (setq mac-command-modifier 'meta)		; Command => Meta
+  (setq mac-option-modifier 'super)		; Option => Super
+  (setq mac-control-modifier 'control)	; Control => Control
+  (setq ns-function-modifier 'hyper))	; Fn => Hyper
+
+(defun external-keyboard ()
+  "Define modifers to suit external keyboard."
+  (interactive)
+  (setq mac-command-modifier 'super)
+  (setq mac-option-modifier 'meta)
+  (setq mac-control-modifier 'control)
+  (setq ns-function-modifier 'none))
+
+;; Because I normally run emacs from the Mac dock, it gets its execution path not from the
+;; shell, but from launchd.
+(setq added-dirs
+	  (list "/Library/TeX/texbin"
+			"/usr/local/bin"
+			"/Users/tom/bin"
+			(s-concat
+			 (s-trim (shell-command-to-string "/opt/local/bin/python-config --prefix"))
+			 "/bin")))
+(dolist (added-dir added-dirs)
+  (add-to-list 'exec-path added-dir))
+
+;; Frame format (emacs-fu.blogspot.com)
+(setq frame-title-format
+      '("" invocation-name ": "(:eval (if (buffer-file-name)
+										  (abbreviate-file-name (buffer-file-name)) "%b"))))
+;; Start up the emacs server process.
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+;; When visiting a log file, enable tailing.
+(add-to-list 'auto-mode-alist '("\\.log\\'" . auto-revert-tail-mode))
+
+;; Allow narrowing to proceed without warning.
+(put 'narrow-to-region 'disabled nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Mode-specific configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;; Magit - http://magit.vc/
+(setq magit-last-seen-setup-instructions "1.4.0")
+
+;;;; Magit Gitflow - https://github.com/jtatarik/magit-gitflow
+(require 'magit-gitflow)
+(add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
+
+;;;; Emmet - https://github.com/smihica/emmet-mode
+(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook 'emmet-mode)
+(eval-after-load "emmet-mode"		;Don't rebind C-j
+  '(define-key emmet-mode-keymap (kbd "C-j") nil))
+
+;;;; Web-Mode - http://web-mode.org/
+(require 'web-mode)
+(setq web-mode-enable-engine-detection t)
+(setq web-mode-engines-alist
+	  '(("ctemplate" . "\\.html\\'")
+		("django" .	"\\.jinja2\\'")
+		("django" . "\\.email\\'")))
+(add-to-list 'auto-mode-alist '("\\.email\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jinja2\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(defun nurk/web-mode-hook ()
+  (setq web-mode-markup-indent-offset 2))
+(add-hook 'web-mode-hook 'nurk/web-mode-hook)
+
+;;;; Helm - https://emacs-helm.github.io/helm/
+(require 'helm-config)
+(helm-mode 1)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-z") 'helm-select-action)
+
+;;;; Projectile - http://batsov.com/projectile/
+(projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+(setq projectile-switch-project-action 'helm-projectile)
+
+;;;; Javacsript JS2 - https://github.com/mooz/js2-mode
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook (lambda () (setq comment-start "// ")))
+(add-hook 'js2-mode-hook #'js2-refactor-mode) ;What is #' ??
+(js2r-add-keybindings-with-prefix "C-c r")
+
+;;;; AUCTeX - https://www.gnu.org/software/auctex/
+(require 'tex-site)
+(setq TeX-auto-save t)					; Enable parse on save.
+(setq TeX-parse-self t)					; Enable parse on load.
+(setq-default TeX-master nil)			; Query for master file.
+(setq TeX-PDF-mode t)					; Default to PDF
+(add-hook 'TeX-mode-hook 'flyspell-mode)
+
+;;;; RefTeX - http://www.gnu.org/software/auctex/reftex.html
+(require 'reftex)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+
+;;;; Org Mode - http://orgmode.org/
+(require 'org-mac-link)
+(add-hook 'org-mode-hook 'flyspell-mode)
+(add-hook 'org-mode-hook (lambda () (define-key org-mode-map (kbd "C-c g") 'org-mac-grab-link)))
+
+;;;; Markdown Mode - http://jblevins.org/projects/markdown-mode/
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;;;; Elpy - https://github.com/jorgenschaefer/elpy
+(elpy-enable)
+(elpy-use-ipython)
+(pyvenv-workon "brook")
+
+;;;; YASnippet - http://capitaomorte.github.io/yasnippet/
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;;;; Miscellaneous
+
+;; Open dash documentation for item at point.
+(require 'dash-at-point)
+
+;; Use semantic mode for the speedbar.
+(require 'semantic/sb)
+
+;; Use the GIT version of ox-reveal
+(add-to-list 'load-path "~/src/org-reveal")
+(require 'ox-reveal)
+
+;; Work around emacs 24.3 ispell bug.
+(add-hook 'TeX-mode-hook (lambda () (setq-local comment-padding " ")))
+(setq ispell-program-name "aspell")
+
+;; Turn on fill-column indicator
+(fci-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Theming
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(load-theme 'zenburn)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Mail
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
+(require 'mu4e)
+
+(add-to-list 'mu4e-view-actions
+			 '("ViewInBrowser" . mu4e-action-view-in-browser) t)
+
+(setq nurk/mu4e-account-list
+      '(("campus"
+		 (user-mail-address "tnurkkala@cse.taylor.edu")
+		 (message-signature-file "cse.txt"))
+		("cse"
+		 (user-mail-address "tnurkkala@cse.taylor.edu")
+		 (message-signature-file "cse.txt"))
+		("gmail"
+		 (user-mail-address "tom.nurkkala@gmail.com")
+		 (message-signature-file "gmail.txt"))
+		("nurknet"
+		 (user-mail-address "tom@nurknet.com")
+		 (message-signature-file "nurknet.txt"))
+		))
+
+(defun nurk/mu4e-set-account ()
+  "Set the account for composing a message."
+  (let* ((account
+          (if mu4e-compose-parent-message
+			  ;; Set account based on parent message (if there is one).
+              (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                (string-match "/\\(.*?\\)/" maildir)
+                (match-string 1 maildir))
+			;; Set account by asking user.
+            (completing-read (format "Compose with account: (%s) "
+                                     (mapconcat #'(lambda (var) (car var))
+                                                nurk/mu4e-account-list "/"))
+                             (mapcar #'(lambda (var) (car var)) nurk/mu4e-account-list)
+                             nil t nil nil (caar nurk/mu4e-account-list))))
+		 ;; Look up variables associated with account.
+         (account-vars (cdr (assoc account nurk/mu4e-account-list))))
+    (if account-vars
+		;; Found the account; set up variables.
+        (mapc #'(lambda (var)
+                  (set (car var) (cadr var)))
+              account-vars)
+      ;; Doh: no such account!
+      (error "No email account found"))))
+
+(add-hook 'mu4e-compose-pre-hook 'nurk/mu4e-set-account)
+
+(defun nurk/mu4e-compose-stuff ()
+  "My settings for message composition."
+  (set-fill-column 72)
+  (flyspell-mode))
+
+(add-hook 'mu4e-compose-mode-hook 'nurk/mu4e-compose-stuff)
+
+(defun nurk/mark-spam (msg ignore)
+  "Mark messages flagged as spam."
+  (with-temp-buffer
+    (insert-file-contents (mu4e-message-field msg :path))
+    (goto-char (point-min))
+    (if (re-search-forward "^X-Spam-Flag: \\(.*\\)" nil t 1)
+		(string= (downcase (match-string 1)) "yes")
+      nil)))
+
+(add-to-list 'mu4e-headers-custom-markers
+			 '("Spam" nurk/mark-spam))
